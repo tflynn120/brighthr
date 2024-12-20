1a55e9ba506020577f5286edea9a5afb7ac75fed
tflynn120 <61735446+tflynn120@users.noreply.github.com>
2024-12-20 10:48:00 +0000

    Set up test file

---
+++ src/__tests/Documents.test.tsx
@@ -0,0 +1,45 @@
+/**
+ * @jest-environment jsdom
+ */
+import "@testing-library/jest-dom";
+
+describe("sorts the documents", () => {
+  beforeEach(() => {
+    // setup
+  });
+
+  it("by name", () => {
+    throw new Error();
+  });
+
+  it("by size", () => {
+    throw new Error();
+  });
+
+  it("by date", () => {
+    throw new Error();
+  });
+});
+
+describe("Filter by filename", () => {
+  beforeEach(() => {
+    // setup
+  });
+  it("does not find a document when no document contains the search value", () => {
+    throw new Error();
+  });
+
+  it("finds a document containing the search value", () => {
+    throw new Error();
+  });
+});
+
+describe("clicking on individual documents", () => {
+  beforeEach(() => {
+    // setup
+  });
+
+  it("shows the correct documents details", () => {
+    throw new Error();
+  });
+});
